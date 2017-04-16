4b4f1e52f36b728532b6b60f0bc9876178e4946b
Dima <gip840@mail.ru>
2017-04-16 15:45:20 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
+#include <Windows.h>
+#include <limits.h>
 
-int main (void)
+
+#include "HaffmanAlgh.h"
+
+
+int main()
 {
-    printf ("Hello, world!\n");
-    return 0;
+	//AAAAABBBBCCCCCCCDDDDDD
+	const char sampleText[] = "AAAAABBBBCCCCCCCDDDDDD";//сюда подается строчка, далее будет реализация работы с файлом
+	unsigned int numberOfNodes = 0;
+	int srtLength = strlen(sampleText) + 1; //debug
+
+	Node * root = CreateNewNode(true, true, false, NULL, NULL, 'R', INT_MAX);
+	numberOfNodes = CreateStartNodes(root, sampleText);
+
+	
+	Node * acc3 = root->next;
+	/*for (int i = 0; i < numberOfNodes + 1; i++)
+	{
+		printf("%c - %i\n", acc3->symbol, acc3->count);
+		acc3 = acc3->next;		
+	}*/
+
+	while (acc3)
+	{
+		printf("%c - %i\n", acc3->symbol, acc3->count);
+		acc3 = acc3->next;
+	}
+	Node * acc4 = CreateTree(root, srtLength);
+	CreateCodeOfSymbols(acc4);
+
+	return 0;
 }
