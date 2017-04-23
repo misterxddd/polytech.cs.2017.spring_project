a5b6bb75d1baf05df82295f2c0f988847d04d852
misterxddd <gip840@mail.ru>
2017-04-23 12:59:25 +0300

    Header

---
+++ src/dynamic_array.h
@@ -0,0 +1,20 @@
+#pragma once
+
+
+typedef struct pop
+{
+	char symbol;
+	struct pop * next;
+}string;
+
+string * CreateString(char * str);
+string * CreateStringStruct(char _symbol, string * _next);
+void PrintString(string * _string);
+
+int Lenght(string * _string);
+void AddToTheEnd(string * _string, char _symbol);
+void DeleteLast(string * _string);
+void DeleteAll(string * _string, int _lenght);
+void ClearString(string * str);
+void CopyString(string * _str1, string * _str2);
+void Sex(string * _str, string * _str1);
