d948b9ffc9de7cfc837bce951547a182e571856a
Dima <gip840@mail.ru>
2017-04-30 13:49:34 +0300

    Delete file.h

--- src/file.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stdio.h>
-
-#include "dynamic_array.h"
-#include "HaffmanAlgh.h"
-
-void OtputArchivedFile(char * _Str, char * _Path, string * _BinStr, Node * _table, int _numberOfNodes);
-byte * LitlleToBigEndian(byte * _val);
-byte * GetSignaHeaderByFile(FILE * _file);
