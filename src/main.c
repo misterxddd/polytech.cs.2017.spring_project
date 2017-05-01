e0b15ba40755d076720f0eb87c7db0741e3ce285
misterxddd <gip840@mail.ru>
2017-05-01 12:08:41 +0300

    Update

--- src/main.c
+++ src/main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <limits.h>
-#include <conio.h>
 #include <string.h>
 #include <stdlib.h>
 
@@ -11,64 +10,62 @@
 
 int main()
 {
-	scanf("%c", &path);//Р’РІРѕРґРёРј РїСѓС‚СЊ Рє РЅР°С€РµРјСѓ С„Р°Р№Р»Сѓ
-	//const char path[] = "C:/Anime/file.bin";
-	int c = 0;
-	scanf("%i", &c);
-	if (c == 1)
-	{
-		FILE * file = fopen(path, "rb");
-		int fileSize = GetFileSize(file);
-		byte * bytes;
-		if (GetSignaHeaderByFile(file) == false && fileSize < 67108864)
-		{
-			bytes = (byte *)malloc(fileSize);
-			if (!bytes)
-			{
-				printf("bomzh!");
-				return 0;
-			}
-			rewind(file);
-			fread(bytes, 1, fileSize, file);
-			fclose(file);
+    //const char path[] = "C:/Anime/1.txt"; //Далее будет реализована нормальная работа
 
-			int numberOfNodes = 0;
-			Node * root = CreateNewNode(true, true, false, NULL, NULL, 'R', INT_MAX);
-			numberOfNodes = CreateStartNodes(root, bytes, fileSize);
+    int c = 0;
+    scanf("%i", &c);
+    if (c == 0)
+    {
+        FILE * file = fopen(path, "rb");
+        int fileSize = GetFileSize(file);
+        byte * bytes;
+        if (GetSignaHeaderByFile(file) == false && fileSize < 67108864)
+        {
+            bytes = (byte *)malloc(fileSize);
+            if (!bytes)
+            {
+                printf("bomzh!");
+                return 0;
+            }
+            rewind(file);
+            fread(bytes, 1, fileSize, file);
+            fclose(file);
 
-			Node * acc4 = CreateTree(root, fileSize);
-			string * codeTable = CreateCodeOfSymbols2(acc4, fileSize);
-			GetAllCodes(codeTable);
-			string * binTExt = GetBinaryText(codeTable, bytes, fileSize);
-			PrintString(binTExt);
+            int numberOfNodes = 0;
+            int lengthOfArchivedBytes = 0;
+            Node * root = CreateNewNode(true, true, false, NULL, NULL, 'R', INT_MAX);
+            numberOfNodes = CreateStartNodes(root, bytes, fileSize);
 
-			OtputArchivedFile(bytes, path, binTExt, root->next, numberOfNodes);
-		}
-	}
-
-	else
-	{
-		FILE * file = fopen(path, "rb");
-		int fileSize = GetFileSize(file);
-		if (GetSignaHeaderByFile(file) == true)
-			printf("Archived\n");
-		Node * root = CreateNewNode(true, true, false, NULL, NULL, 'R', INT_MAX);
-		Node * copyRoot = root;
-		Node * acc = NULL;
-		int siska = 0;
-		int strLength = CreateStartNodesByArchived(file, root, &siska);
-		acc = CreateTree(copyRoot, strLength);
-		byte * data = (byte *)malloc(siska);
-		fseek(file, fileSize - siska, SEEK_SET);
-		fread(data, 1, siska, file);
-		// printf("%s",Unarchive(acc, data , strLength,siska));
-		byte * buff = (byte *)malloc(strLength);
-		buff = Unarchive(acc, data, strLength, siska);
-		fclose(file);
-		FILE * file1 = fopen(path, "wb");
-		fwrite(buff, 1, strLength, file1);
-		fclose(file1);
-	}
-	_getch();
+            Node * acc4 = CreateTree(root, fileSize);
+            string * codeTable = CreateCodeOfSymbols2(acc4, fileSize);
+           // GetAllCodes(codeTable);
+            byte * archivedBytes = GetArchivedBytes(codeTable, bytes, fileSize,&lengthOfArchivedBytes);
+           // PrintString(binTExt);
+            OtputArchivedFile(archivedBytes, path, root->next, numberOfNodes, lengthOfArchivedBytes);
+        }
+    }
+    if (c == 1)
+    { 
+        FILE * file = fopen(path, "rb");
+        int fileSize = GetFileSize(file); 
+        if (GetSignaHeaderByFile(file) ==  true)
+            printf("Archived\n");
+        Node * root = CreateNewNode(true, true, false, NULL, NULL, 'R', INT_MAX);
+        Node * copyRoot = root;
+        Node * acc = NULL;
+        int siska = 0;
+        int strLength = CreateStartNodesByArchived(file,root,&siska);
+        acc = CreateTree(copyRoot, strLength);
+        byte * data = (byte *)malloc(siska);
+        fseek(file, fileSize - siska, SEEK_SET);
+        fread(data, 1, siska, file);
+       // printf("%s",Unarchive(acc, data , strLength,siska));
+        byte * buff = (byte *)malloc(strLength);
+        buff = Unarchive(acc, data, strLength, siska);
+        fclose(file);
+        FILE * file1 = fopen(path, "wb");
+        fwrite(buff, 1, strLength, file1);
+        fclose(file1);
+    }
 	return 0;
 }
