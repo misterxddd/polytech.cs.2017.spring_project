ff684ae3533fcc220750357dc5d375339e18c59b
VachugovaK <vachugova.katya@mail.ru>
2017-05-12 00:57:35 +0300

    Update files.h

--- src/files.h
+++ src/files.h
@@ -2,8 +2,8 @@
 #include "dynamic_array.h"
 #include "HaffmanAlgh.h"
 
-void OtputArchivedFile(byte * _ArchivedBytes, char * _Path, Node * _table, int _numberOfNodes, int _countOfArchivedBytes);
-int CreateStartNodesByArchived(FILE * _file, Node * _root, int * _counter);
-bool GetSignaHeaderByFile(FILE * _file); 
-byte * Unarchive(Node * _root, byte * _archivedBlock, int _sizeOfBlock, int _sizeOfArchived);
-int GetFileSize(FILE * _file);
+void OtputArchivedFile(byte * _ArchivedBytes, char * _Path, Node * _table, int _numberOfNodes, int _countOfArchivedBytes); //Функция кладет заархивированные элементы в файл
+int CreateStartNodesByArchived(FILE * _file, Node * _root, int * _counter); //Воссоздание узлов
+bool GetSignaHeaderByFile(FILE * _file); //Проверка на наличие сигнатуры
+byte * Unarchive(Node * _root, byte * _archivedBlock, int _sizeOfBlock, int _sizeOfArchived); //Процесс разархивации
+int GetFileSize(FILE * _file); //Вычисление размера файла
