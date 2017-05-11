61238ea52c30d4623ddbe102d66954c10aa99fae
VachugovaK <vachugova.katya@mail.ru>
2017-05-11 09:53:16 +0300

    Comments

--- src/files.h
+++ src/files.h
@@ -4,6 +4,6 @@
 
 void OtputArchivedFile(byte * _ArchivedBytes, char * _Path, Node * _table, int _numberOfNodes, int _countOfArchivedBytes);
 int CreateStartNodesByArchived(FILE * _file, Node * _root, int * _counter);
-bool GetSignaHeaderByFile(FILE * _file);
+bool GetSignaHeaderByFile(FILE * _file); 
 byte * Unarchive(Node * _root, byte * _archivedBlock, int _sizeOfBlock, int _sizeOfArchived);
 int GetFileSize(FILE * _file);
