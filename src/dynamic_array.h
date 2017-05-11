fad9a132db44d2657e17a643a0113a02316cb07d
VachugovaK <vachugova.katya@mail.ru>
2017-05-11 09:51:47 +0300

    comment

--- src/dynamic_array.h
+++ src/dynamic_array.h
@@ -6,14 +6,13 @@ typedef struct pop
 	struct pop * next;
 }string;
 
-string * CreateString(byte * _str);
-string * CreateStringStruct(byte _symbol, string * _next);
-void PrintString(string * _string);
+string * CreateString(byte * _str); //Создание ячейки односвязного списка
+string * CreateStringStruct(byte _symbol, string * _next); //Создание самого списка
 
-int Lenght(string * _string);
-void AddToTheEnd(string * _string, byte _symbol);
-void DeleteLast(string * _string);
-void ClearString(string * str);
-void CopyString(string * _str1, string * _str2);
-char GetByIndex(string * _str, int _index);
-void DeleteString(string * str);
+int Lenght(string * _string); //Нахождение длины списка
+void AddToTheEnd(string * _string, byte _symbol); //Добавление элемента в конец списка
+void DeleteLast(string * _string); //Удаление последнего элемента 
+void ClearString(string * str); //Очистка односвязного списка 
+void CopyString(string * _str1, string * _str2); //Копирование элементов из первого во второй
+char GetByIndex(string * _str, int _index); //Получение эллемента по i-ому индексу 
+void DeleteString(string * str); //Удаление списка
