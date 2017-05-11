e071eb0e6e5a654d5941fe512ce4cef24911a04e
misterxddd <gip840@mail.ru>
2017-05-11 09:08:48 +0300

    Optimizzation

--- src/HaffmanAlgh.c
+++ src/HaffmanAlgh.c
@@ -18,34 +18,28 @@ Node * CreateNewNode(bool _isChar,bool _isNeed, Node * _left, Node * _rigth,Node
 	return newNode;
 }
 
-int CreateStartNodes(Node * root, byte * byteArray,int size)
+int CreateStartNodes(Node * root, byte * byteArray, int size)
 {
-	int i, j;
-	byte k;
-	Node * acc = root;
-	int numberOfNodes = 0;
-	for (k = 0; k < MAX_SYMBOL_COUNT; k++)
-	{
-		int counter = 0;
-		for (j = 0; j < size; j++)
-		{
-			if (byteArray[j] == k)
-				counter++;
-		}
-		if (counter != 0)
-		{
-			acc = root;
-			for (i = 0; i < numberOfNodes; i++)
-			{
-				acc = acc->next;
-			}
-			numberOfNodes++;
-			acc->next = CreateNewNode(true, true, NULL, NULL, NULL, k, counter);
-		}
-        if(k == 0xff)
-            return numberOfNodes;
-	}
+    Node * acc = root;
+    int numberOfNodes = 0;
+    int entropyOfSymbols[256];
+    memset(entropyOfSymbols, 0, 256 * 4);
+    for (int i = 0; i < size; i++)
+    {
+        entropyOfSymbols[byteArray[i]]++;
+    }
+    for (int i = 0; i < 256; i++)
+    {
+        if (entropyOfSymbols[i] != 0)
+        {
+            acc->next = CreateNewNode(true, true, NULL, NULL, NULL, i, entropyOfSymbols[i]);
+            numberOfNodes++;
+            acc = acc->next;
+        }
+    }
+    return numberOfNodes;
 }
+
 
 Node * CreateTree(Node * root, int arraySize)
 {	
@@ -53,10 +47,20 @@ Node * CreateTree(Node * root, int arraySize)
 	Node * acc2;
 	Node * acc3;
 	Node * acc4 = NULL;
+
+    acc3 = root->next;
+
+    while (acc3)
+    {
+        acc4 = acc3;
+        acc3 = acc3->next;
+    }
+
 	gt:
 	acc1 = root;
 	acc2 = root;
 	acc3 = root->next;
+
 	while (acc3)
 	{
 		if ((acc1->count > acc3->count) && (acc3->isNeed == true))
@@ -75,119 +79,12 @@ Node * CreateTree(Node * root, int arraySize)
 	acc2->isNeed = false;
 	acc3 = root->next;
 	
-		while (acc3)
-		{
-			acc4 = acc3;
-			acc3 = acc3->next;
-		}
-		acc4->next = CreateNewNode(false, true, acc1, acc2, NULL, 0, acc1->count + acc2->count);
-		if ((acc1->count + acc2->count) == arraySize)
-			return acc4->next;
-		goto gt;	
-}
-
-string * CreateCodeOfSymbols(Node * root, int strLen)
-{
-	string * str = (string *)malloc(sizeof(string) * 256);
-	memset(str, 0, sizeof(string) * 256); // debug
-	string * buf = CreateStringStruct(0,NULL);
-	Node * stackNodes[100]; //WARNING!!! Buffer overflow!
-	Node * copyRoot = root;
-	int index = 0;
-	stackNodes[index] = root;
-	direction direct = LEFT;
-	while (true)
-	{
-	gt:
-		if (root->left == NULL && root->rigth == NULL && root == copyRoot)
-			return str;
-		if(root->left == NULL && root->rigth == NULL && root->isChar == false)
-		{
-			root = stackNodes[index - 1];
-			if (direct == LEFT)
-				root->left = NULL;
-			else
-				root->rigth = NULL;
-			root = stackNodes[0];
-			index = 0;
-			ClearString(buf);
-			goto gt;
-		}
 		
-		if (root->left == NULL && root->rigth != NULL)
-		{
-			direct = !direct;
-		}
-		if (root->left != NULL && root->rigth == NULL)
-		{
-			direct = !direct;
-		}
-		if (root->isChar == true)
-		{
-			CopyString(buf->next, str + *(byte *)(&root->symbol));
-			ClearString(buf);
-
-            if (stackNodes[index - 1] == copyRoot)
-            {
-                if (direct == LEFT)
-                    copyRoot->left = NULL;
-                else
-                    copyRoot->rigth = NULL;
-
-                if (root->left == NULL && root->rigth == NULL && root == copyRoot)
-                    return str;
-
-                index = 0;
-                root = stackNodes[index];
-                goto gt;
-            }
-
-			index--;
-			root = stackNodes[index];
-			if (direct == LEFT)
-				root->left = NULL;
-			else
-				root->rigth = NULL;
-
-			if (root->left == NULL && root->rigth == NULL && root == copyRoot)
-				return str;
-				
-			index = 0;
-			root = stackNodes[index];
-			goto gt;
-		}
-		if (direct == LEFT)
-		{
-			AddToTheEnd(buf, '0');
-			if (root->left != NULL)
-				root = root->left;
-			else
-			{
-				index = 0;
-				root = stackNodes[index];
-				direct = !direct;
-				ClearString(buf);
-			}
-			index++;
-			stackNodes[index] = root;			
-		}
-		if (direct == RIGTH)
-		{
-			AddToTheEnd(buf, '1');
-			if(root->rigth != NULL)
-			root = root->rigth;
-			else
-			{
-				index = 0;
-				root = stackNodes[index];
-				direct = !direct;
-				ClearString(buf);
-			}
-			index++;
-			stackNodes[index] = root;			
-		}
-	}
-
+	acc4->next = CreateNewNode(false, true, acc1, acc2, NULL, 0, acc1->count + acc2->count);
+    acc4 = acc4->next;
+	if ((acc1->count + acc2->count) == arraySize)
+		return acc4;
+	goto gt;	
 }
 
 string * GetCodeOfSymbol(string * table, byte symbol)
@@ -195,47 +92,43 @@ string * GetCodeOfSymbol(string * table, byte symbol)
 	return table + symbol;
 }
 
-
 byte * GetArchivedBytes(string * table, byte * bytes, int length, int * lengthOfArchivedBytes)
 {
-	/*string * binstr = CreateStringStruct(0, NULL);
-	for (int i = 0; i < length; i++)
-	{
-	string * temp = CreateStringStruct(0, 0);
-	CopyString(GetCodeOfSymbol(table, bytes[i]),temp);
-	Sex(binstr, temp);
-	}
-	return binstr->next;*/
+    byte * archivedBytes = (byte *)malloc(length);
+    int * lengthsOfCodesOfSymbols = (int *)malloc(4 * 256);
+    memset(archivedBytes, 0, length); 
+    string * buff;
 
-	byte * archivedBytes = (byte *)malloc(length);
-	memset(archivedBytes, 0, length);
-	string * buff = CreateStringStruct(0, 0);
-	int i = 0;
-	int index = 0;
-	int count = 0;
-	while (i < length)
-	{
-		CopyString(GetCodeOfSymbol(table, bytes[i]), buff);
-		//PrintString(buff);f
-		int j = 0;
-		for (j = 0; j < Lenght(buff); j++)
-		{
-			archivedBytes[index] = archivedBytes[index] | (GetByIndex(buff, j) - 48) << (7 - count);
-			count++;
-			if (count == 8)
-			{
-				count = 0;
-				index++;
-			}
-		}
-		ClearString(buff);
-		i++;
-	}
-	if ((length % 8) != 0)
-		*lengthOfArchivedBytes = index + 1;
-	else
-		*lengthOfArchivedBytes = index;
-	return archivedBytes;
+    for (int k = 0; k <= 0xff; k++)
+    {
+        buff = table + k;
+        *(lengthsOfCodesOfSymbols + k) = Lenght(buff);
+    }
+
+    int i = 0;
+    int index = 0;
+    int count = 0;
+    while (i < length)
+    {
+        buff = table + bytes[i];
+        for (int j = 0; j < *(lengthsOfCodesOfSymbols + bytes[i]); j++)
+        {
+            archivedBytes[index] = archivedBytes[index] | GetByIndex(buff,j) << (7 - count);
+            count++;
+            if (count == 8)
+            {
+                count = 0;
+                index++;
+            }     
+        }   
+        i++;
+    }
+    if((length % 8) != 0)
+        *lengthOfArchivedBytes = index + 1;
+    else
+        *lengthOfArchivedBytes = index;
+    free(lengthsOfCodesOfSymbols);
+    return archivedBytes;
 }
 
 
@@ -256,7 +149,7 @@ void GetAllCodes(string * table)
 }
 //END FOR DEBUG
 
-string * CreateCodeOfSymbols2(Node * root, int strLen)
+string * CreateCodeOfSymbols(Node * root, int strLen)
 {
     string * code = CreateStringStruct(0, 0);
     string * str = (string *)malloc(sizeof(string) * 256);
@@ -269,27 +162,17 @@ void BuildTable(Node * root, string * str, string * code)
 {
     if (root->left != NULL)
     {
-        printf("0");
-        AddToTheEnd(code, '0');
+        AddToTheEnd(code, 0);
         BuildTable(root->left, str, code);
     }
     if (root->rigth != NULL)
     {
-        printf("1");
-        AddToTheEnd(code, '1');
+        AddToTheEnd(code, 1);
         BuildTable(root->rigth, str, code);
     }
     if (root->isChar)
     {
-        printf("%c\n", root->symbol);
         CopyString(code->next, str + *(byte *)(&root->symbol));
-        //ClearString(code);
     }
-    //DeleteLast(code);
-
-    //if (Lenght(code) > 1)
-    //{
-        //printf("|");
         DeleteLast(code);
-    //}
 }
