619562caa6b4407da5edc1e3f0876d76168c75ff
misterxddd <gip840@mail.ru>
2017-05-11 09:09:57 +0300

    Optimiz

--- src/main.c
+++ src/main.c
@@ -9,46 +9,54 @@
 #include "files.h"
 
 
-int main()
+int main(int argc, char * argv[])
 {
-    const char path[] = "C:/Anime/1.txt"; //Далее все будет нормально :)
+    FILE * file = fopen(argv[1], "rb");
+    int fileSize = GetFileSize(file);
+    byte * bytes;
 
-    int c = 0;
-    scanf("%i", &c);
-    if (c == 0)
+    if ((GetSignaHeaderByFile(file) == false) && fileSize <= 268435456)
     {
-        FILE * file = fopen(path, "rb");
-        int fileSize = GetFileSize(file);
-        byte * bytes;
-        if (GetSignaHeaderByFile(file) == false && fileSize < 67108864)
+        bytes = (byte *)malloc(fileSize);
+        if (!bytes)
         {
-            bytes = (byte *)malloc(fileSize);
-            if (!bytes)
-            {
-                printf("bomzh!");
-                return 0;
-            }
-            rewind(file);
-            fread(bytes, 1, fileSize, file);
-            fclose(file);
-
-            int numberOfNodes = 0;
-            int lengthOfArchivedBytes = 0;
-            Node * root = CreateNewNode(true, true, false, NULL, NULL, 'R', INT_MAX);
-            numberOfNodes = CreateStartNodes(root, bytes, fileSize);
-
-            Node * acc4 = CreateTree(root, fileSize);
-            printf("Tree created");
-            string * codeTable = CreateCodeOfSymbols2(acc4, fileSize);
-            //GetAllCodes(codeTable);
-            byte * archivedBytes = GetArchivedBytes(codeTable, bytes, fileSize,&lengthOfArchivedBytes);
-           // PrintString(binTExt);
-            OtputArchivedFile(archivedBytes, path, root->next, numberOfNodes, lengthOfArchivedBytes);
+            printf("bomzh!");
+            return 0;
         }
+        rewind(file);
+        fread(bytes, 1, fileSize, file);
+        fclose(file);
+
+        int numberOfNodes = 0;
+        int lengthOfArchivedBytes = 0;
+        Node * root = CreateNewNode(true, true, NULL, NULL, NULL, 'R', INT_MAX);
+        numberOfNodes = CreateStartNodes(root, bytes, fileSize);
+
+        Node * acc4 = CreateTree(root, fileSize);
+        //printf("Tree created");
+        string * codeTable = CreateCodeOfSymbols(acc4, fileSize);
+        //GetAllCodes(codeTable);
+        byte * archivedBytes = GetArchivedBytes(codeTable, bytes, fileSize, &lengthOfArchivedBytes);
+        // PrintString(binTExt);
+        OtputArchivedFile(archivedBytes, argv[1], root->next, numberOfNodes, lengthOfArchivedBytes);
+
+        free(bytes);
+        free(archivedBytes);
+        while (acc4)
+        {
+            acc4 = root->next;
+            free(root);
+            root = acc4;
+        }
+        for (int i = 0; i < 256; i++)
+        {
+            ClearString(codeTable + i);
+        }
+        free(codeTable);
     }
-    if (c == 1)
+    else
     { 
-        FILE * file = fopen(path, "rb");
+        FILE * file = fopen(argv[1], "rb");
         int fileSize = GetFileSize(file); 
         if (GetSignaHeaderByFile(file) ==  true)
             printf("Archived\n");
@@ -61,14 +69,12 @@ int main()
         byte * data = (byte *)malloc(siska);
         fseek(file, fileSize - siska, SEEK_SET);
         fread(data, 1, siska, file);
-       // printf("%s",Unarchive(acc, data , strLength,siska));
         byte * buff = (byte *)malloc(strLength);
         buff = Unarchive(acc, data, strLength, siska);
         fclose(file);
-        FILE * file1 = fopen(path, "wb");
+        FILE * file1 = fopen(argv[1], "wb");
         fwrite(buff, 1, strLength, file1);
         fclose(file1);
     }
-	//_getch();
 	return 0;
 }
